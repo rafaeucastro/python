8e9de32a7534e9b01202043212eb0f7e55aaee63
Rafaeu Castro <rafael.scastro@dataprev.gov.br>
2025-05-21 18:31:13 -0300

    Added sqlite database and update and delete endpoints

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI, HTTPException, Query, Path, Body
-from music import MusicForm, Music, Genre
-from typing import Optional
+from fastapi import FastAPI, HTTPException, Query, Path, Depends
+from models import MusicForm, Music, Genre, MusicUpdate
+from contextlib import asynccontextmanager
+from db import create_db_and_tables, get_session
+from sqlmodel import Session, select
+from exceptions.notfound import NotFoundException
 
-api = FastAPI()
+#https://fastapi.tiangolo.com/advanced/events/?h=life#lifespan
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 music_list = [
     Music(id=0, name="Butter", artist="BTS", genre=Genre.K_POP),
@@ -16,19 +25,24 @@ music_list = [
     Music(id=8, name="You", artist="Regard, Troye Sivan, Tate McRae", genre=Genre.ELECTRONIC),
     Music(id=9, name="Titanium", artist="David Guetta feat. Sia", genre=Genre.ELECTRONIC),
 ]
-    
-@api.get('/music/{id}', response_model=Music)  
-async def get_music_by_id(id: int = Path(description="music unique identification")):
-    music = next((m for m in music_list if m.id == id), None)
+
+#https://fastapi.tiangolo.com/tutorial/path-params/?h=pydant    
+@app.get('/music/{id}', response_model=Music)  
+async def get_music_by_id(
+    id: int = Path(description="Music Unique Identification"), 
+    session: Session = Depends(get_session)
+):
+    music = session.get(Music, id)
     if(music == None): raise HTTPException(status_code=404)
     return music
 
-@api.get('/music', response_model=list[Music])  
-async def get_music_by_genre_and_name(
+@app.get('/music', response_model=list[Music])  
+async def get_musics(
     genre: Genre = Query(None, description="Filter by genre"), 
-    name: str = Query(None, description="Search by name")
+    name: str = Query(None, description="Search by name"),
+    session: Session = Depends(get_session)
 ):
-    results = music_list
+    results = session.exec(select(Music)).all()
     if genre:
         results = [music for music in results if music.genre == genre.value]
     if name: 
@@ -36,10 +50,42 @@ async def get_music_by_genre_and_name(
 
     return results
 
-@api.post('/music', response_model=Music)
-async def add_music(form: MusicForm):
-    global next_id
-    next_id = 10
-    new_music = Music(id=next_id, **form.model_dump())
-    music_list.append(new_music)
-    return form
+@app.post('/music', response_model=Music)
+async def add_music(form: MusicForm, session: Session = Depends(get_session)):
+    new_music = Music(**form.model_dump())
+    session.add(new_music)
+    session.commit()
+    session.refresh(new_music)
+    return new_music
+
+@app.put('/music/{id}', response_model=Music)
+async def update_music(
+    form: MusicUpdate,
+    session: Session = Depends(get_session),
+    id: int = Path(description="Unique identifier of the music to be updated"),
+):
+    music = session.get(Music, id)
+
+    if not music:
+        raise NotFoundException()
+
+    music_data = form.model_dump(exclude_unset=True)
+    music.sqlmodel_update(music_data)
+    session.add(music)
+    session.commit()
+    session.refresh(music)
+    return music
+
+@app.delete('/music/{id}')
+async def delete_music(
+    session: Session = Depends(get_session),
+    id: int = Path(description="Unique identifier of the music to be removed"),
+):
+    music = session.get(Music, id)
+
+    if not music:
+        raise NotFoundException()
+
+    session.delete(music)
+    session.commit()
+    return {"message": f"The music with ID: {id} was succesfully removed!"}
